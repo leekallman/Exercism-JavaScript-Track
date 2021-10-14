53867b2f4566f3c8d0f8350dcd6a88e89c38aec3
Lisa Lee <lisaleekallman@gmail.com>
2021-10-14 16:44:52 +0200

    repetition twoFer

--- two-fer/two-fer.js
+++ two-fer/two-fer.js
@@ -1,23 +1,8 @@
-//
-// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-
-
-// export const twoFer = (name = 'Alice') => {
-//   if (name) {
-//     return `One for ${name}, one for me.`;
-//   } else {
-//     return "One for you, one for me"
-//   }
-// };
-
 export const twoFer = (name) => {
   if (name) {
     return `One for ${name}, one for me.`
   } else {
     return `One for you, one for me.`
+
   }
 }
-
-
-twoFer("Thulani")
