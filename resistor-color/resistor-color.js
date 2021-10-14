130187091999dcddd8b89b1f1d6e92f4f30e1e53
Lisa Lee <lisaleekallman@gmail.com>
2021-10-14 20:31:07 +0200

    repetition of resistor-color

--- resistor-color/resistor-color.js
+++ resistor-color/resistor-color.js
@@ -1,14 +1,9 @@
-//
-// This is only a SKELETON file for the 'Resistor Color' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+//create a array with all colors in
+//create a function that return the numeric value/index of a given color
 
-// 1. create a list of the band colors
-// 2. function that fetch the value of the given color
-
-
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
+export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
 export const colorCode = (color) => {
-    return COLORS.indexOf(color);
+    let index = COLORS.indexOf(color);
+    return index
 }
