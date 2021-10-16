b004ca480ffb204715e43f8fc3ec0f892299aec9
Lisa Lee <lisaleekallman@gmail.com>
2021-10-16 13:19:53 +0200

    repetition + solved the last test

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,15 +1,9 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+const decodedValue = (colorArray) => {
+  if (colorArray.length > 2) {
+    colorArray.pop();
+  }
+  return + colorArray.map(color => COLORS.indexOf(color)).join('')
+}
 
-export const decodedValue = () => {
-  const color1 = "brown"
-  const color2 = "black"
-  const indexOfFirst = COLORS.indexOf(color1)
-  const indexOfSecond = COLORS.indexOf(color2)
-  return indexOfFirst, indexOfSecond
-};
-export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"];
-
-decodedValue()
+export { COLORS, decodedValue }
